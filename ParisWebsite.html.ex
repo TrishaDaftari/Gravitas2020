e6e98886d117f0dca1ed9b14fb3dd354507d9970
TrishaDaftari <72310444+TrishaDaftari@users.noreply.github.com>
2020-10-03 23:05:28 +0530

    Add files via upload

---
+++ ParisWebsite.html.ex
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head> 
+<title> PARIS AT A GLANCE </title>
+
+</head>
+<body text="red">
+<h1> PARIS AT A GLANCE </h1>
+<img src="paris.jpg" align="centre">
+<h2> <font color="purple">
+Paris is located in northern central France, in a north-bending arc of the river Seine whose crest includes two islands, the Île Saint-Louis and the larger Île de la Cité, which form the oldest part of the city.
+The Parisii, a sub-tribe of the Celtic Senones, inhabited the Paris area from around the middle of the 3rd century BC. One of the area's major north–south trade routes crossed the Seine on the île de la Cité; this meeting place of land and water trade routes gradually became an important trading centre.The Parisii traded with many river towns (some as far away as the Iberian Peninsula) and minted their own coins for that purpose. It is the most expensive city of the world. The city is a major railway, highway and air-transport hub served by two international airports: Paris–Charles de Gaulle (the second busiest airport in Europe) and Paris–Orly. Opened in 1900, the city's subway system, the Paris Métro, serves 5.23 million passengers daily; it is the second busiest metro system in Europe after the Moscow Metro.
+Paris is the capital and most populous city of France, with an estimated population of 2,148,271 residents as of 2020, in an area of 105 square kilometres (41 square miles). Since the 17th century, Paris has been one of Europe's major centres of finance, diplomacy, commerce, fashion, science and arts.
+</h2>
+<h3> <font color="green"> Paris is famous for its innumerable tourist spots about which you can learn more at <a href="ParisWebsitepage2.html.ex">ClickHere</a>
+</h3>
+</body>
+</html>
